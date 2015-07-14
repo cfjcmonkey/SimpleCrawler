1d873fb14098f5092589f8b8db040a0a7ee39cd8
cfjcmonkey <cfjmonkey@gmail.com>
2015-07-14 17:37:40 +0800

    Update simple_js_linkedin_all.py
    In a OO way.

--- simple_js_linkedin_all.py
+++ simple_js_linkedin_all.py
@@ -16,164 +16,135 @@ from multiprocessing import Pool
 import sys
 import datetime
 
-
-prevent_list = {};
-down_list = [];
-unzip_list = [];
-treate_list = [];
-down_path = '';
-treate_path_part = '';
-keep_path = '';
-idx = 0;
-driver = ""
-countfinish = 0
-hosturl = "http://www.linkedin.com"
-logFile = open('log.txt', 'a');
-
-def LoadDriver():
-	global driver
-#	dcap = dict(DesiredCapabilities.PHANTOMJS)
-#	dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0 ")
-#	driver = webdriver.PhantomJS(desired_capabilities=dcap)
-	driver = webdriver.Chrome()
-
 class down_info(object):
-	def __init__(self,du,sp,lv):
-		self.down_url = du;
-		self.save_path = sp;
-		self.level = lv;
-#self.context = ""
+  def __init__(self,du,sp,lv):
+    self.down_url = du;
+    self.save_path = sp;
+    self.level = lv;
 
-def finish_callback(url):
-	global countfinish
-	countfinish += 1
-	if countfinish % 30 == 0:
-		print 'finish %d'%countfinish
-	fobj = open("finishurl_linkedin_dir.dat","a")
-	fobj.write(url + '\n')
-	fobj.close()
-	print '/////////finish url %s/////////'%url
+class HumanizedCrawler:  
 
-def AddTargetUrl(url):
-	fobj = open("url_linkedIn_com_all.txt", "a")
-	fobj.write(url + '\n')
-	fobj.close()
-	
-def download_js(di,sec,isLeaf):
-	if not di:
-		return ""
+  def __init__(self, urlFile, hosturl, down_path = '', preventFile = ''):
+    self.hosturl = hosturl;
+    self.urlFile = urlFile;
+    self.down_path = down_path;
+    if self.down_path == '':
+      self.down_path = 'output/';
+    if(not os.path.exists(self.down_path)):
+      os.makedirs(self.down_path)
 
-	write_log("trying to download " + di.down_url);
-	global prevent_list;
-	global down_path;
-	global idx;
-	global driver
-	global logFile
+    if preventFile == '':
+      preventFile = 'finish_url.dat'; 
+    self.preventFile = open(preventFile, 'a');
+    self.logFile = open('log.txt', 'a');
+    self.mineUrlFile = open('newUrl.dat', 'a');
 
-	try:
-		if (not isLeaf and di.down_url in prevent_list) or (isLeaf and os.path.isfile(di.save_path)):
-			write_log("already download " + di.down_url)
-			return di.down_url
-		idx += 1
-		if idx % 50 == 0:
-			logFile.flush();
-#			driver.close()
-#			LoadDriver()
+    self.prevent_list = {};
+    self.driver = self.LoadDriver();
+    self.idx = 0;
+    self.countfinish = 0;
 
-		driver.get(di.down_url)
+    if os.path.exists(self.preventFile):
+      self.prevent_list = [line.strip() for line in file(preventFile)]
 
-		sleeptime = random.randint(1, sec)
-		sleep(sleeptime)
-		
-		doc = driver.page_source.encode('utf-8');
-		
-		fobj = open(di.save_path , 'w')
-		fobj.write(doc+"\n")
-		fobj.close()
-		
-		finish_callback(di.down_url)
-		
-		if not isLeaf:
-			bg = doc.find("<ul class=\"column dual-column\">")
-			ed = doc.find("</ul>", bg)
-			dircnt = doc[bg:ed]
-			count = 0
-			for m in re.finditer(r'\shref="(.+?)"\s', dircnt):
-				count += 1
-				href = m.group(1)
-				if href.startswith('http') == False:
-					href = hosturl + href
-				if '/directory/' in href:
-					dj = down_info(href, down_path + href[href.rfind('/'):] + ".html", di.level + 1)
-					download_js(dj, sec, False)
-				else:
-					AddTargetUrl(href)
-			write_log('mining link %d in %s'%(count, di.down_url))
-		return di.down_url
-	except:
-		write_log("download error")
-		raise Exception("Download Error")
+  def Close():
+    self.driver.close()
+    self.logFile.close();
+    self.mineUrlFile.close();
+    self.preventFile.close();
 
-def write_log(info):
-	global logFile
-	
-	print datetime.datetime.now(), info
-	logFile.write('%s %s\n'%(datetime.datetime.now(), info));
+  def LoadDriver(self):
+  # dcap = dict(DesiredCapabilities.PHANTOMJS)
+  # dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0 ")
+  # driver = webdriver.PhantomJS(desired_capabilities=dcap)
+    self.driver = webdriver.Chrome()
 
-def main_dir():
-	global down_path;
-	global prevent_list;
-	global countfinish;
-	global driver
-	global logFile
+  def finish_callback(self, url):
+    self.countfinish += 1
+    if self.countfinish % 30 == 0:
+      print 'finish %d'%self.countfinish    
+    self.preventFile.write(url + '\n')
+    print '/////////finish url %s/////////'%url
 
-	keep_path = os.getcwd()+'/'
-	down_path = keep_path + 'linkedin/'
-	if(not os.path.exists(down_path)):
-		os.makedirs(down_path)
+  def write_log(self, info):  
+    print datetime.datetime.now(), info
+    self.logFile.write('%s %s\n'%(datetime.datetime.now(), info));
 
-	LoadDriver()
-	prevent_list = [line.strip() for line in file("finishurl_linkedin_dir.dat")]
+  def AddTargetUrl(self, url):
+    self.mineUrlFile.write(url + '\n')
+  
+  def download_js(di,sec,isLeaf):
+    if not di:
+      return ""
 
-#	rooturl = "http://www.linkedin.com/directory/companies?trk=hb_ft_companies_dir"	
-#	di =  down_info(rooturl, down_path + rooturl[rooturl.rfind('/'):] + ".html", 2)
-#	download_js(di, 3, False)
+    self.write_log("trying to download " + di.down_url);
 
-	rootlist = [line.strip() for line in file('url_linkedIn_root.txt')]
-	for rooturl in rootlist:
-		di =  down_info(rooturl, down_path + rooturl[rooturl.rfind('/'):] + ".html", 2)
-		download_js(di, 5, False)
+    try:
+      if (not isLeaf and di.down_url in self.prevent_list) or (isLeaf and os.path.isfile(di.save_path)):
+        write_log("already download " + di.down_url)
+        return di.down_url
 
-	print "total task url ", countfinish
-	driver.close()
-	logFile.close();
+      self.idx += 1
+      if self.idx % 50 == 0:
+        self.logFile.flush();
+  #     driver.close()
+  #     LoadDriver()
 
-def main_page():
-	global down_path;
-	global prevent_list;
-	global countfinish;
-	global driver
-	global logFile
+      self.driver.get(di.down_url)
 
-	keep_path = os.getcwd()+'/'
-	down_path = keep_path + 'linkedin/'
-	if(not os.path.exists(down_path)):
-		os.makedirs(down_path)
-	LoadDriver()
-	
-	rootlist = [line.strip() for line in file('url_linkedIn_com_all.txt')]
-	for rooturl in rootlist:
-		di =  down_info(rooturl, down_path + rooturl[rooturl.rfind('/'):] + ".html", 2)
-		download_js(di,5, True)
+      sleeptime = random.randint(1, sec)
+      sleep(sleeptime)
+      
+      doc = self.driver.page_source.encode('utf-8');
+      
+      fobj = open(di.save_path , 'w')
+      fobj.write(doc+"\n")
+      fobj.close()
+      
+      self.finish_callback(di.down_url)
+      
+      if not isLeaf:
+        self.AnalyzePage(doc, di.level);
 
-	print "total task url ", countfinish
-	driver.close()
-	logFile.close()
+      return di.down_url
+    except:
+      self.write_log("download error")
+      raise Exception("Download Error")
+
+  def AnalyzePage(self, doc, level):
+    bg = doc.find("<ul class=\"column dual-column\">")
+    ed = doc.find("</ul>", bg)
+    dircnt = doc[bg:ed]
+    count = 0
+    for m in re.finditer(r'\shref="(.+?)"\s', dircnt):
+      count += 1
+      href = m.group(1)
+      if href.startswith('http') == False:
+        href = hosturl + href
+      if '/directory/' in href:
+        dj = down_info(href, down_path + href[href.rfind('/'):] + ".html", level + 1)
+        self.download_js(dj, sec, False)
+      else:
+        self.AddTargetUrl(href)
+    self.write_log('mining link %d in %s'%(count, di.down_url))
+
+  def DownloadDirectory():
+    rootlist = [line.strip() for line in file(self.urlFile)]
+    for rooturl in rootlist:
+      di =  down_info(rooturl, down_path + rooturl[rooturl.rfind('/'):] + ".html", 2)
+      self.download_js(di, 5, False)
+
+    print "total task url ", countfinish
+
+  def DownloadPage():    
+    rootlist = [line.strip() for line in file(self.urlFile)]
+    for rooturl in rootlist:
+      di =  down_info(rooturl, down_path + rooturl[rooturl.rfind('/'):] + ".html", 2)
+      self.download_js(di,5, True)
+
+    print "total task url ", countfinish
+
 
 
 if __name__=="__main__" :
-	main_page();
-
-
-
-
+  main_page();
